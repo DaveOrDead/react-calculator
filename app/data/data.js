d32c75de0924c40ec36dcb05af21ba38d4dc8606
DaveOrDead <davidajberner@gmail.com>
2016-08-07 08:05:16 +0100

    add initial data

---
+++ app/data/data.js
@@ -0,0 +1,30 @@
+const keys = [1,2,3,4,5,6,7,8,9,0];
+
+const calculatorFunctions = [
+    {
+        value: 'Clear',
+        icon: 'A/C'
+    },
+    {
+        value: 'Divide',
+        icon: '/'
+    },
+    {
+        value:'Multiply',
+        icon: 'x'
+    },
+    {
+        value: 'Subtract',
+        icon: '-'
+    },
+    {
+        value:'Add',
+        icon: '+'
+    },
+    {
+        value: 'Equals',
+        icon: '='
+    }];
+
+export keys;
+export calculatorFunctions;
